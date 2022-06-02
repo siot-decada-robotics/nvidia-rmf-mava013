39496c8a5f73f308d3f4023605f996333f84ddab
Edan Toledo <e.toledo@instadeep.com>
2022-06-02 16:12:22 +0200

    add blank init files

--- mava/systems/jax/mamcts/__init__.py
+++ mava/systems/jax/mamcts/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Jax MAPPO system."""
+"""Jax MAMCTS system."""
 from mava.systems.jax.mamcts.components.executing.action_selection import (
     MCTSFeedforwardExecutorSelectAction,
 )
