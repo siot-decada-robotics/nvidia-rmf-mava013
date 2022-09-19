0658e525c965c8037d80743b59efdeb87db35985
AsadJeewa <asad.jeewa@gmail.com>
2022-09-19 12:50:04 +0200

    chore: refactor offline exmple file to ippo

--- examples/jax/debugging/simple_spread/feedforward/decentralised/run_ippo_log_offline.py
+++ examples/jax/debugging/simple_spread/feedforward/decentralised/run_ippo_log_offline.py
@@ -13,9 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Example running MAPPO on debug MPE environments."""
+"""Example running IPPO on debug MPE environments."""
 import functools
-import os
 from datetime import datetime
 from typing import Any
 
@@ -23,12 +22,10 @@ import optax
 from absl import app, flags
 
 from mava.components.jax.executing.offline import EvaluatorOfflineLogging
-from mava.systems.jax import mappo
+from mava.systems.jax import ippo
 from mava.utils.environments import debugging_utils
 from mava.utils.loggers import logger_utils
 
-# Without this flag, JAX uses the whole GPU from the beginning and our trainer crashes.
-os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 FLAGS = flags.FLAGS
 flags.DEFINE_string(
     "env_name",
@@ -64,8 +61,8 @@ def main(_: Any) -> None:
 
     # Networks.
     def network_factory(*args: Any, **kwargs: Any) -> Any:
-        return mappo.make_default_networks(  # type: ignore
-            policy_layer_sizes=(254, 254, 254),
+        return ippo.make_default_networks(  # type: ignore
+            policy_layer_sizes=(256, 256, 256),
             critic_layer_sizes=(512, 512, 256),
             *args,
             **kwargs,
@@ -85,13 +82,15 @@ def main(_: Any) -> None:
         time_delta=log_every,
     )
 
-    # Optimizer.
-    optimizer = optax.chain(
+    # Optimisers.
+    policy_optimiser = optax.chain(
         optax.clip_by_global_norm(40.0), optax.scale_by_adam(), optax.scale(-1e-4)
     )
-
+    critic_optimiser = optax.chain(
+        optax.clip_by_global_norm(40.0), optax.scale_by_adam(), optax.scale(-1e-4)
+    )
     # Create the system.
-    system = mappo.MAPPOSystem()
+    system = ippo.IPPOSystem()
     system.update(EvaluatorOfflineLogging)
 
     # Build the system.
@@ -100,7 +99,8 @@ def main(_: Any) -> None:
         network_factory=network_factory,
         logger_factory=logger_factory,
         experiment_path=experiment_path,
-        optimizer=optimizer,
+        policy_optimiser=policy_optimiser,
+        critic_optimiser=critic_optimiser,
         run_evaluator=True,
         sample_batch_size=5,
         num_epochs=15,
