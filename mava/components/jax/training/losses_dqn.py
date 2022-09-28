fix: added priority calculation and table updating.

mava/components/jax/training/losses_dqn.py
@@ -26,7 +26,7 @@ import rlax
 
 from mava.components.jax.training.base import Loss
 from mava.core_jax import SystemTrainer
-
+from acme.agents.jax.dqn import learning_lib
 
 @dataclass
 class MADQNLossConfig:
@@ -70,6 +70,8 @@ class MADQNLoss(Loss):
             actions: Dict[str, jnp.ndarray],
             discounts: Any,
             rewards: Any,
+            probs: Any,
+            keys: Any
         ) -> Tuple[Dict[str, jnp.ndarray], Dict[str, Dict[str, jnp.ndarray]]]:
             """Gradient of loss.
 
@@ -86,10 +88,12 @@ class MADQNLoss(Loss):
                 grads: gradients of loss with respect to all the parameters.
                 extra: extra information.
             """
-
+            #self.reverb_update = Any
             grads = {}
             loss = {}
             loss_info = {}
+            t1 = {}
+            reverb_update = {}
             for agent_key in trainer.store.trainer_agents:
                 agent_net_key = trainer.store.trainer_agent_net_keys[agent_key]
                 network = trainer_network[agent_net_key]
@@ -130,11 +134,31 @@ class MADQNLoss(Loss):
                     td_error = batch_error(
                         q_tm1, actions, r_t, d_t, q_t_value, q_t_selector
                     )
-                    loss = jnp.mean(rlax.l2_loss(td_error))
-                    loss_info = {"loss_total": loss}
-                    return loss, loss_info
 
-                (loss[agent_key], loss_info[agent_key]), grads[
+                    #TODO(SIDDARTH): MAKE SEPARATE LOSS FUNCTIONS
+                    #FOR PRIORITIZD EXO
+                    #Importance weighting
+                    importance_weights = (1. / probs).astype(jnp.float32)
+                    importance_weights **= 1
+                    importance_weights /= jnp.max(importance_weights)
+
+                    #print("*************IMPORTANCE********************")
+                    #print("*************IMPORTANCE********************")
+                    #print("*************IMPORTANCE********************")
+                    #print("*************IMPORTANCE********************")
+                    #print("*************IMPORTANCE********************")
+                    
+                    
+                    loss = jnp.mean(rlax.l2_loss(td_error))
+                    # Reweight
+                    reverb_update = learning_lib.ReverbUpdate(
+                        keys=keys, priorities=jnp.abs(td_error).astype(jnp.float64))
+
+                    self.reverb_update =reverb_update
+                    loss_info = {"loss_total": loss}
+                    return loss, [loss_info,reverb_update]
+                #(loss[agent_key], loss_info[agent_key],reverb_update), grads[
+                (loss[agent_key], [loss_info[agent_key],reverb_update[agent_key]]), grads[
                     agent_key
                 ] = jax.value_and_grad(loss_fn, has_aux=True)(
                     params[agent_net_key],
@@ -146,9 +170,11 @@ class MADQNLoss(Loss):
                     rewards[agent_key],
                     next_observations[agent_key].legal_actions,
                 )
+                #loss_info,reverb_update = data
+                loss_info = loss_info[agent_key]
                 loss_info["total_loss"] = loss[agent_key]
 
-            return grads, loss_info
+            return grads, loss_info, reverb_update
 
         # Save the gradient function.
         trainer.store.grad_fn = jax.jit(

mava/components/jax/training/model_updating_dqn.py
@@ -17,6 +17,7 @@
 from dataclasses import dataclass
 from typing import Any, Callable, Dict, Optional, Tuple
 
+from acme.jax import utils
 import jax
 import jax.numpy as jnp
 import optax
@@ -26,7 +27,7 @@ from optax._src import base as optax_base
 
 from mava.components.jax.training import BatchDQN, Utility
 from mava.core_jax import SystemTrainer
-
+import tree
 
 @dataclass
 class MADQNMinibatchUpdateConfig:
@@ -153,18 +154,18 @@ class MADQNEpochUpdate(Utility):
 
         @jax.jit
         def model_update_epoch(
-            carry: Tuple[KeyArray, Any, Any, Any, optax.OptState, BatchDQN],
+            carry: Tuple[Any, Any, KeyArray, Any, Any, Any, optax.OptState, BatchDQN],
             unused_t: Tuple[()],
         ) -> Tuple[
             Tuple[KeyArray, Any, Any, optax.OptState, BatchDQN],
             Dict[str, jnp.ndarray],
         ]:
             """Performs model updates based on one epoch of data."""
-            key, params, target_params, opt_states, batch, steps = carry
+            key, params, target_params, opt_states, batch, steps, probs,keys = carry
             #jax.debug.print(steps)
-            #exit()
+           
             # Calculate the gradients and agent metrics.
-            gradients, agent_metrics = trainer.store.grad_fn(
+            gradients, agent_metrics, reverb_updates = trainer.store.grad_fn(
                 params,
                 target_params,
                 batch.observations,
@@ -172,8 +173,25 @@ class MADQNEpochUpdate(Utility):
                 batch.actions,
                 batch.discounts,
                 batch.rewards,
+                probs,
+                keys
             )
 
+            #TODO: Finish assigning priorities
+            keys, priorities = tree.map_structure(
+                # Fetch array and combine device and batch dimensions.
+                lambda x: utils.fetch_devicearray(x).reshape((-1,) + x.shape[2:]),
+                (reverb_updates.keys, reverb_updates.priorities))
+            print(keys)
+            print(priorities)
+            trainer.store.data_server_client.mutate_priorities(table = "trainer")#,updates=dict(zip(keys, priorities)))
+            print("***********REPLAY*******************")
+            print("***********REPLAY*******************")
+            print("***********REPLAY*******************")
+            print("***********REPLAY*******************")
+            print("***********REPLAY*******************")
+            print(trainer.store.data_server_client)
+            exit()
             # Update the networks and optimizers.
             metrics = {}
             new_params = {}

mava/components/jax/training/step_dqn.py
@@ -57,11 +57,12 @@ class MADQNStep(Step):
         @jit
         @chex.assert_max_traces(n=1)
         def sgd_step(
-            states: TrainingStateDQN, sample: reverb.ReplaySample
+            states: TrainingStateDQN, sample: reverb.ReplaySample,
         ) -> Tuple[TrainingStateDQN, Dict[str, jnp.ndarray]]:
             """Performs a minibatch SGD step, returning new state and metrics."""
             # Extract the data.
             data = sample.data
+            keys, probs, *_ = sample.info
 
             observations, new_observations, actions, rewards, discounts, _ = (
                 data.observations,
@@ -103,6 +104,8 @@ class MADQNStep(Step):
                     states.opt_states,
                     batch,
                     states.steps,
+                    probs,
+                    keys,
                 ),
                 {},
             )
@@ -140,7 +143,7 @@ class MADQNStep(Step):
                 utils.batch_concat(rewards, num_batch_dims=0)
             )
 
-            return new_states, metrics
+            return new_states,metrics
 
         def step(sample: reverb.ReplaySample) -> Tuple[Dict[str, jnp.ndarray]]:
 
@@ -155,9 +158,10 @@ class MADQNStep(Step):
             }
             opt_states = trainer.store.opt_states
             random_key, _ = jax.random.split(trainer.store.key)
-
+            #keys, probs, *_ = sample.info
+            
             steps = trainer.store.trainer_counts["trainer_steps"]
-
+            
             states = TrainingStateDQN(
                 params=params,
                 target_params=target_params,