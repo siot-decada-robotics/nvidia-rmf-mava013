b47669b86d4223b5b8401018dd0d25ed317b7925
Sasha Abramowitz <reallysasha@gmail.com>
2022-10-24 11:30:32 +0200

    chore: moved new components inside dqn system

--- mava/components/jax/training/__init__.py
+++ mava/components/jax/training/__init__.py
@@ -29,7 +29,8 @@ from mava.components.jax.training.losses import (
     MAPGWithTrustRegionClippingLoss,
     MAPGWithTrustRegionClippingLossSeparateNetworks,
 )
-from mava.components.jax.training.losses_dqn import MADQNLoss
+
+# from mava.systems.jax.madqn.components.training.loss import MADQNLoss
 from mava.components.jax.training.model_updating import (
     MAPGEpochUpdate,
     MAPGEpochUpdateSeparateNetworks,
@@ -37,19 +38,12 @@ from mava.components.jax.training.model_updating import (
     MAPGMinibatchUpdateSeparateNetworks,
 )
 
-# FOR MADQN
-from mava.components.jax.training.model_updating_dqn import (
-    MADQNEpochUpdate,
-    MADQNMinibatchUpdate,
-)
 from mava.components.jax.training.step import (
     DefaultTrainerStep,
     MAPGWithTrustRegionStep,
     MAPGWithTrustRegionStepSeparateNetworks,
 )
 
-# included for MADQN
-from mava.components.jax.training.step_dqn import MADQNStep
 from mava.components.jax.training.trainer import (
     BaseTrainerInit,
     CustomTrainerInit,
---
+++ mava/systems/jax/madqn/components/training/__init__.py
@@ -0,0 +1,3 @@
+from .loss import MADQNLoss
+from .model_updating import MADQNEpochUpdate, MADQNMinibatchUpdate
+from .step import MADQNStep
